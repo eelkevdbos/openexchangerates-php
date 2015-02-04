738a30f0915da9135d1b27019593211a5290a973
Eelke van den Bos <eelkevdbos@gmail.com>
2015-02-04 15:24:26 +0100

    examples executable in commandline with first argument app_id

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
-$app_id = 'YOUR_APP_ID_HERE';
+$app_id = $argv[1];
 $base_url = Api::getBaseUrl(false); //with FALSE retrieves HTTP url, defaults to HTTPS
 
 $client = new OpenExchangeRates(

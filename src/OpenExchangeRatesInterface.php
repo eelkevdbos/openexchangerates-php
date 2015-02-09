519d822e2f0b5e60184f2444622cd42da1246032
Eelke van den Bos <eelkevdbos@gmail.com>
2015-02-09 03:02:58 +0100

    return types to interface

--- src/OpenExchangeRatesInterface.php
+++ src/OpenExchangeRatesInterface.php
@@ -1,19 +1,36 @@
 <?php namespace EvdB\OpenExchangeRates;
 
-
 interface OpenExchangeRatesInterface
 {
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function latest();
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function currencies();
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function historical($date);
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function timeSeries($startDate, $endDate);
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function convert($value, $from, $to);
 
+    /**
+     * @return \GuzzleHttp\Stream\StreamInterface|null
+     */
     public function jsonp($method, array $args, $callback);
 
 }
